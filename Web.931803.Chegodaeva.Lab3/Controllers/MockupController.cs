0474efc2e896cbebb8adba1e826c973c7048a53a
Elizaveta <42829488+sexypotato@users.noreply.github.com>
2021-05-27 16:26:33 +0700

    init

---
+++ Web.931803.Chegodaeva.Lab3/Controllers/MockupController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using Web._931803.Chegodaeva.Lab3.Models;
+
+
+namespace Web._931803.Chegodaeva.Lab3.Controllers
+{
+    public class MockupController : Controller
+    {
+        private static readonly Random random = new Random();
+        private static readonly QuizModel quiz = new QuizModel();
+
+        public IActionResult Quiz()
+        {
+            Generate();
+            return View();
+        }
+
+        public IActionResult QuizResult()
+        {
+            ViewBag.ListResult = quiz.ListResult;
+            ViewBag.RightAnswersCount = quiz.RightAnswersCount;
+            ViewBag.AnswersCount = quiz.AnswersCount;
+
+            return View();
+        }
+
+        public IActionResult Next(string value1, string value2, string answer, string operation)
+        {
+            if (answer == null) return View("Error");
+            else
+            {
+                ConvertAndCalculate(value1, value2, answer, operation);
+                Generate();
+                return View("Quiz");
+            }
+           
+
+        }
+
+        public IActionResult Result(string value1, string value2, string answer, string operation)
+        {
+            if (answer != null) ConvertAndCalculate(value1, value2, answer, operation);            
+                
+            ViewBag.ListResult = quiz.ListResult;
+            ViewBag.RightAnswersCount = quiz.RightAnswersCount;
+            ViewBag.AnswersCount = quiz.AnswersCount;
+            return View("QuizResult");
+            
+        }
+
+        public void Generate()
+        {
+            ViewBag.num1 = random.Next(11);
+            ViewBag.num2 = random.Next(11);
+            ViewBag.operation = random.Next(2) == 1 ? "+" : "-";
+        }
+
+        public void ConvertAndCalculate(string num1, string num2, string answer, string operation)
+        {
+            int Num1 = Convert.ToInt32(num1);
+            int Num2 = Convert.ToInt32(num2);
+            int Answer = Convert.ToInt32(answer);
+
+            quiz.ListResult.Add(Num1 + " " + operation + " " + Num2 + " = " + Answer);
+            if ((operation == "+" && Num1 + Num2 == Answer) ||
+                (operation == "-" && Num1 - Num2 == Answer))
+                quiz.RightAnswersCount++;
+            quiz.AnswersCount++;
+        }
+    }
+}
---
+++ Web.931803.Chegodaeva.Lab3/Models/QuizModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Web._931803.Chegodaeva.Lab3.Models
+{
+    public class QuizModel
+    {
+        public List<string> ListResult { get; set; } = new List<string>();
+        public int RightAnswersCount { get; set; } = 0;
+        public int AnswersCount { get; set; } = 0;
+    }
+}
